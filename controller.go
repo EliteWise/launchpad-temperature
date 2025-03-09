fbe3a2d25fd8bcb20fe1dbe75c9351adad67d033
Florent <florent.mogenet@gmail.com>
2025-03-09 18:29:33 +0100

    v1: display temp 3x5 with input city / clearing

--- controller.go
+++ controller.go
@@ -4,11 +4,88 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"os/signal"
 	"regexp"
 	"strings"
+	"syscall"
 
 	"github.com/rakyll/portmidi"
 )
+
+// 5 l x 3 col
+var digits = map[rune][5][3]bool{
+	'0': {
+		{true, true, true},
+		{true, false, true},
+		{true, false, true},
+		{true, false, true},
+		{true, true, true},
+	},
+	'1': {
+		{false, true, false},
+		{false, true, false},
+		{false, true, false},
+		{false, true, false},
+		{false, true, false},
+	},
+	'2': {
+		{true, true, true},
+		{false, false, true},
+		{true, true, true},
+		{true, false, false},
+		{true, true, true},
+	},
+	'3': {
+		{true, true, true},
+		{false, false, true},
+		{true, true, true},
+		{false, false, true},
+		{true, true, true},
+	},
+	'4': {
+		{true, false, true},
+		{true, false, true},
+		{true, true, true},
+		{false, false, true},
+		{false, false, true},
+	},
+	'5': {
+		{true, true, true},
+		{true, false, false},
+		{true, true, true},
+		{false, false, true},
+		{true, true, true},
+	},
+	'6': {
+		{true, true, true},
+		{true, false, false},
+		{true, true, true},
+		{true, false, true},
+		{true, true, true},
+	},
+	'7': {
+		{true, true, true},
+		{false, false, true},
+		{false, true, false},
+		{true, false, false},
+		{true, false, false},
+	},
+	'8': {
+		{true, true, true},
+		{true, false, true},
+		{true, true, true},
+		{true, false, true},
+		{true, true, true},
+	},
+	'9': {
+		{true, true, true},
+		{true, false, true},
+		{true, true, true},
+		{false, false, true},
+		{true, true, true},
+	},
+}
 
 func getTemperature(city string) string {
 	requestUrl := fmt.Sprintf("http://wttr.in/%s?format=%%t", city)
@@ -25,32 +102,80 @@ func getTemperature(city string) string {
 	return strings.TrimSpace(re.FindAllString(string(respBody), -1)[0])
 }
 
+func sendMidiMessage(stream *portmidi.Stream, status, data1, data2 int64) {
+	stream.WriteShort(status, data1, data2)
+}
+
+func displayDigit(stream *portmidi.Stream, digit rune, xOffset int) {
+	pattern, exists := digits[digit]
+	if !exists {
+		return
+	}
+
+	for row := 0; row < 5; row++ {
+		for col := 0; col < 3; col++ {
+			if pattern[row][col] {
+				note := 81 - (row * 10) + col + (xOffset * 4)
+				sendMidiMessage(stream, 0x90, int64(note), 5)
+			}
+		}
+	}
+}
+
+func displayTemperature(stream *portmidi.Stream, temp string) {
+	xOffset := 0
+	for _, digit := range temp {
+		displayDigit(stream, digit, xOffset)
+		xOffset += 1
+	}
+}
+
+func clearLaunchpad(stream *portmidi.Stream) {
+	for note := 11; note <= 88; note++ {
+		sendMidiMessage(stream, 0x90, int64(note), 0)
+	}
+}
+
 func main() {
+	city := "Paris"
+	if len(os.Args) > 1 {
+		city = os.Args[1]
+	}
+
 	portmidi.Initialize()
 	defer portmidi.Terminate()
 
 	deviceID := -1
 	for i := 0; i < portmidi.CountDevices(); i++ {
 		info := portmidi.Info(portmidi.DeviceID(i))
-		fmt.Println(info.Name)
-		if info.IsOutputAvailable && info.Name == "Launchpad MK2 MIDI 1" {
+		if info.IsOutputAvailable && strings.Contains(info.Name, "Launchpad") {
 			deviceID = i
 			break
 		}
 	}
 
 	if deviceID == -1 {
-		fmt.Println("Launchpad non trouvé.")
+		fmt.Println("Launchpad not found.")
 		return
 	}
 
 	out, err := portmidi.NewOutputStream(portmidi.DeviceID(deviceID), 1024, 0)
 	if err != nil {
-		fmt.Println("Erreur ouverture du port MIDI:", err)
+		fmt.Println("Error opening MIDI port.", err)
 		return
 	}
 	defer out.Close()
 
-	temp := getTemperature("Montpellier")
+	temp := getTemperature(city)
 	fmt.Println(temp)
+
+	clearLaunchpad(out)
+	displayTemperature(out, temp)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigChan
+
+	clearLaunchpad(out)
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 Turn your launchpad into a temperature displayer!
 
-Run `go run .` to start displaying.
+Run `go run . Paris` (any city) to start displaying.
